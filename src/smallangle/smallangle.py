93be4b9210cebecf150a2fc892367f53dca3590a
DanadeMunck <149485890+DanadeMunck@users.noreply.github.com>
2023-11-17 14:49:57 +0100

    added click

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,18 +1,26 @@
 import click
 import numpy as np
-#from numpy import pi
+from numpy import pi
 import pandas as pd
 
-
+@click.group()
+def cmd_group():
+    pass
+@cmd_group.command()
+@click.option("--number",
+    default=0, )
 def sin(number):
-    x = np.linspace(0, 2 * np.pi, number)
+    x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
 
+@cmd_group.command()
+@click.option("--number",
+    default = 0,)
 def tan(number):
-    x = np.linspace(0, 2 * np.pi, number)
+    x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
