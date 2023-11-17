3d223b181e4c3f35199e4877a854c6668201cc80
DanadeMunck <149485890+DanadeMunck@users.noreply.github.com>
2023-11-17 14:28:00 +0100

    package geinstalleerd in nieuw environment

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,18 +1,18 @@
 import click
 import numpy as np
-from numpy import pi
+#from numpy import pi
 import pandas as pd
 
 
 def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+    x = np.linspace(0, 2 * np.pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
 
 def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+    x = np.linspace(0, 2 * np.pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
